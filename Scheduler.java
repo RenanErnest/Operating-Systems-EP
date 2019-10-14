eb72089a5bc7caa4de06f33b58835656ee333886
BrunoLCunha <56527452+BrunoLCunha@users.noreply.github.com>
2019-10-14 13:25:33 -0300

    Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -1,1 +1,12 @@
+import java.util.Collections;
+import java.util;
 
+ArrayList<BCP> runningProcessTable = new ArrayList<BCP>(); // Store all BCP references
+  
+PriorityQueue<Object> ready = new PriorityQueue<Object>(); // Create an empty priority queue for ready process
+Queue<Object> blocked = new LinkedList<Object>(); // Simple FIFO for blocked process
+
+Object alternateProcess()
+{
+  ready.poll(); // Return the top priority process
+}
