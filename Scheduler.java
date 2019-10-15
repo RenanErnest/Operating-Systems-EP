daf68038883c38f7a71b60e345e78c0a03a854b0
BrunoLCunha <56527452+BrunoLCunha@users.noreply.github.com>
2019-10-15 00:24:16 -0300

    Update Scheduler.java

--- Scheduler.java
+++ Scheduler.java
@@ -5,8 +5,9 @@ File priorityFile, quantumFile;
 File [] processFiles;
 
 ArrayList<BCP> runningProcessTable = new ArrayList<BCP>(); // Store all BCP references
-PriorityQueue<Object> ready = new PriorityQueue<Object>(); // Create an empty priority queue for ready process
+PriorityQueue<BCP> ready = new PriorityQueue<BCP>(); // Create an empty priority queue for ready process
 Queue<Object> blocked = new LinkedList<Object>(); // Simple FIFO for blocked process
+int X, Y, quantum;
 
 public static void main(String [] args)
 {
@@ -22,9 +23,17 @@ public static void main(String [] args)
 void Run()
 {
   // Run a process quantum times then return
+  String instruction;
+  int aux = 0;
+  // Get instruction  
+  if((aux = instruction.indexOf('=')) != -1)
+  {
+    if(instruction.charAt(0) == 'X') X = Character.getNumericValue(instruction.charAt(2));
+    else Y = Character.getNumericValue(instruction.charAt(2));
+  }
 }
 
-Object AlternateProcess()
+BCP AlternateProcess()
 {
   return ready.poll(); // Return the top priority process
 }
@@ -34,11 +43,15 @@ void Init()
   priorityFile = new File("prioridades.txt"); 
   quantumFile = new File("quantum.txt");  
   BufferedReader br = new BufferedReader(new FileReader(priorityFile)); 
+  BufferedReader qbr = new BufferedReader(new FileReader(quantumFile)); 
+  quantum = Integer.parseInt(qbr.readLine());
   
-  for(int i = 1; i < 11; i++) // Read each process
+  for(int i = 1; i < 11; i++) // Read each command block
   {
     String index = (i < 10 ? "0" : "") + i;
     processFiles[i] = new File(index + ".txt");  
     BCP newProcess = new BCP(index, br.readLine()); // Create BCP with name and priority of the process 
+    runningProcessTable.add(newProcess); // The process is running
+    ready.add(newProcess); // The new process is ready to execute
   }
 }
