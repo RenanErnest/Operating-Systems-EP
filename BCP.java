132fd14ef46fdd3ff2ae405448433dbf0c6ee7e9
BrunoLCunha <56527452+BrunoLCunha@users.noreply.github.com>
2019-10-15 11:56:15 -0300

    Update BCP.java

--- BCP.java
+++ BCP.java
@@ -1,9 +1,9 @@
-int PC;
-int processStatus;
-int priority;
-int X, Y;
-int textSegmentIndex;
-String programName;
+public int PC;
+public int processStatus;
+public int priority;
+public int X, Y;
+public int textSegmentIndex;
+public String programName;
 
 public BCP(String name, int priority)
 {
