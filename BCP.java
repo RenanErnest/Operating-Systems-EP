42bef73ff430a3c28680d6a4cfc5dacd5ff53c48
BrunoLCunha <56527452+BrunoLCunha@users.noreply.github.com>
2019-10-16 00:52:40 -0300

    Update BCP.java

--- BCP.java
+++ BCP.java
@@ -3,12 +3,15 @@ public class BCP{
   public int processStatus;
   public int priority;
   public int X, Y;
+  public int blockedCounter;
   public int textSegmentIndex;
   public String programName;
+  public int credits;
 
   public BCP(String name, int priority, int textSegmentIndex)
   {
     this.priority = priority;
+    this.credits = priority;
     programName = name;
     this.textSegmentIndex = textSegmentIndex;
   }
