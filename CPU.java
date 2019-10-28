7e45137ce52bbe3b33b7f32fbba73cf96b7ef158
Renan <51809247+RenanErnest@users.noreply.github.com>
2019-10-28 01:40:28 -0300

    Some tests and logs

--- CPU.java
+++ CPU.java
@@ -5,7 +5,7 @@ import java.io.*;
 class CPU{
     public static void main(String [] args)
     {   
-        for(int quantum = 1; quantum < 11; quantum++) {
+        for(int quantum = 1; quantum < 21; quantum++) {
             String output = Escalonador.Execution(quantum);
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("log" + quantum + ".txt"), "UTF-8"))) {
                 writer.write(output);
